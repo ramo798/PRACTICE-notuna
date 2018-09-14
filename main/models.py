78b3753e7e10f7a7ddb82ad49b68adecbc0ab71b
ramo <knht31i@gmail.com>
2018-09-14 15:51:30 +0900

    暫定formだけど改善しなあかん

--- main/models.py
+++ main/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Userinfor(models.Model):
     userid = models.CharField(max_length=20)
     profname = models.CharField(max_length=20)
-    profpic = models.TextField()
+    profpic = models.CharField(max_length=20, blank = True)
     proftext = models.CharField(max_length=100)
+    usiusi = models.CharField(max_length=20, blank = True)
 
 class Post(models.Model):
     userids = models.CharField(max_length=20)
